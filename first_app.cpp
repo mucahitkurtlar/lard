cb7dd5c2a980aa6c103327a354202b64aa39059f
Mucahit KURTLAR <mucahit@tutanota.com>
2021-07-07 12:33:53 +0300

    implement renderer to application

--- first_app.cpp
+++ first_app.cpp
@@ -13,17 +13,16 @@
 namespace lard {
 
     struct SimplePushConstantData {
-        glm::mat2 transform{1.f};
+        glm::mat2 transform{ 1.f };
         glm::vec2 offset;
         alignas(16) glm::vec3 color;
     };
-    
+
 
     FirstApp::FirstApp() {
         loadGameObjects();
         createPipelineLayout();
-        recreateSwapChain();
-        createCommandBuffers();
+        createPipeline();
     }
 
     FirstApp::~FirstApp() {
@@ -33,11 +32,17 @@ namespace lard {
     void FirstApp::run() {
         while (!lardWindow.shouldClose()) {
             glfwPollEvents();
-            drawFrame();
+            if (auto commandBuffer = lardRenderer.beginFrame()) {
+                lardRenderer.beginSwapChainRenderPass(commandBuffer);
+                renderGameObjects(commandBuffer);
+                lardRenderer.endSwapChainRenderPass(commandBuffer);
+                lardRenderer.endFrame();
+            }
+
         }
 
         vkDeviceWaitIdle(lardDevice.device());
-        
+
     }
 
     void FirstApp::loadGameObjects() {
@@ -48,10 +53,10 @@ namespace lard {
             {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
             {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}
         };
-        
+
         //std::vector<LardModel::Vertex> vertices{};
         //sierpinski(vertices, 5, {-0.5f, 0.5f}, {0.5f, 0.5f}, {0.0f, -0.5f});
-        
+
         auto lardModel = std::make_shared<LardModel>(lardDevice, vertices);
 
         auto triangle = LardGameObject::createGameObject();
@@ -66,9 +71,9 @@ namespace lard {
         std::vector<LardModel::Vertex> vertices{
             {{0.0f, -0.5f}, {1.0f, 0.0f, 0.0f}},
             {{0.5f, 0.5f}, {0.0f, 1.0f, 0.0f}},
-            {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}}};
+            {{-0.5f, 0.5f}, {0.0f, 0.0f, 1.0f}} };
         auto lardModel = std::make_shared<LardModel>(lardDevice, vertices);
-    
+
         std::vector<glm::vec3> colors{
             {1.f, .7f, .73f},
             {1.f, .87f, .73f},
@@ -77,7 +82,7 @@ namespace lard {
             {.73, .88f, 1.f}  //
         };
         for (auto& color : colors) {
-            color = glm::pow(color, glm::vec3{2.2f});
+            color = glm::pow(color, glm::vec3{ 2.2f });
         }
         for (int i = 0; i < 40; i++) {
             auto triangle = LardGameObject::createGameObject();
@@ -107,12 +112,11 @@ namespace lard {
     }
 
     void FirstApp::createPipeline() {
-        assert(lardSwapChain != nullptr && "Cannot create pipeline before swap chain");
         assert(pipelineLayout != nullptr && "Cannot create pipeline before pipeline layout");
 
         PipelineConfigInfo pipelineConfig{};
         LardPipeline::defaultPipelineConfigInfo(pipelineConfig);
-        pipelineConfig.renderPass = lardSwapChain->getRenderPass();
+        pipelineConfig.renderPass = lardRenderer.getSwapChainRenderPass();
         pipelineConfig.pipelineLayout = pipelineLayout;
         lardPipeline = std::make_unique<LardPipeline>(
             lardDevice,
@@ -121,102 +125,16 @@ namespace lard {
             pipelineConfig);
     }
 
-    void FirstApp::recreateSwapChain() {
-        auto extent = lardWindow.getExtent();
-        while (extent.width == 0 || extent.height == 0) {
-            extent = lardWindow.getExtent();
-            glfwWaitEvents();
-        }
-
-        vkDeviceWaitIdle(lardDevice.device());
-
-        if (lardSwapChain == nullptr) {
-           lardSwapChain = std::make_unique<LardSwapChain>(lardDevice, extent);
-        } else {
-           lardSwapChain = std::make_unique<LardSwapChain>(lardDevice, extent, std::move(lardSwapChain));
-           if (lardSwapChain->imageCount() != commandBuffers.size()) {
-               freeCommandBuffers();
-               createCommandBuffers();
-           }
-           
-        }
-        createPipeline();
-    }
-
-    void FirstApp::createCommandBuffers() {
-        commandBuffers.resize(lardSwapChain->imageCount());
-
-        VkCommandBufferAllocateInfo allocInfo{};
-        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
-        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;
-        allocInfo.commandPool = lardDevice.getCommandPool();
-        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
-
-        if (vkAllocateCommandBuffers(lardDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS)
-        {
-            throw std::runtime_error("Failed to allocate command buffers!");
-        }
-    }
-
-    void FirstApp::freeCommandBuffers() {
-        vkFreeCommandBuffers(lardDevice.device(), lardDevice.getCommandPool(), static_cast<float>(commandBuffers.size()), commandBuffers.data());
-        commandBuffers.clear();
-    }
-
-    void FirstApp::recordCommandBuffer(int imageIndex) {
-        VkCommandBufferBeginInfo beginInfo{};
-        beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
-
-        if (vkBeginCommandBuffer(commandBuffers[imageIndex], &beginInfo) != VK_SUCCESS) {
-            throw std::runtime_error("Failed to begin recording command buffer!");
-        }
-
-        VkRenderPassBeginInfo renderPassInfo{};
-        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-        renderPassInfo.renderPass = lardSwapChain->getRenderPass();
-        renderPassInfo.framebuffer = lardSwapChain->getFrameBuffer(imageIndex);
-
-        renderPassInfo.renderArea.offset = {0, 0};
-        renderPassInfo.renderArea.extent = lardSwapChain->getSwapChainExtent();
-
-        std::array<VkClearValue, 2> clearValues{};
-        clearValues[0].color = {0.01f, 0.01f, 0.01f, 1.0f};
-        clearValues[1].depthStencil = {1.0f, 0};
-        renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
-        renderPassInfo.pClearValues = clearValues.data();
-
-        vkCmdBeginRenderPass(commandBuffers[imageIndex], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-        
-        VkViewport viewport{};
-        viewport.x = 0.0f;
-        viewport.y = 0.0f;
-        viewport.width = static_cast<float>(lardSwapChain->getSwapChainExtent().width);
-        viewport.height = static_cast<float>(lardSwapChain->getSwapChainExtent().height);
-        viewport.minDepth = 0.0f;
-        viewport.maxDepth = 1.0f;
-        VkRect2D scissor{{0, 0}, lardSwapChain->getSwapChainExtent()};
-        vkCmdSetViewport(commandBuffers[imageIndex], 0, 1, &viewport);
-        vkCmdSetScissor(commandBuffers[imageIndex], 0, 1, &scissor);
-
-        renderGameObjects(commandBuffers[imageIndex]);
-
-
-        vkCmdEndRenderPass(commandBuffers[imageIndex]);
-        if (vkEndCommandBuffer(commandBuffers[imageIndex]) != VK_SUCCESS) {
-            throw std::runtime_error("Failed to record command buffer!");
-        }
-    }
-
     void FirstApp::renderGameObjects(VkCommandBuffer commandBuffer) {
         int i = 0;
         for (auto& obj : gameObjects) {
             i += 1;
             obj.transform2d.rotation = glm::mod<float>(obj.transform2d.rotation + 0.0001f * i, 2.f * glm::pi<float>());
         }
-        
+
         lardPipeline->bind(commandBuffer);
 
-        for (auto &obj: gameObjects) {
+        for (auto& obj : gameObjects) {
             obj.transform2d.rotation = glm::mod(obj.transform2d.rotation + 0.001f, glm::two_pi<float>());
 
             SimplePushConstantData push{};
@@ -227,47 +145,6 @@ namespace lard {
             vkCmdPushConstants(commandBuffer, pipelineLayout, VK_SHADER_STAGE_VERTEX_BIT | VK_SHADER_STAGE_FRAGMENT_BIT, 0, sizeof(SimplePushConstantData), &push);
             obj.model->bind(commandBuffer);
             obj.model->draw(commandBuffer);
-        }
-    }
-
-    void FirstApp::drawFrame() {
-        uint32_t imageIndex;
-        auto result = lardSwapChain->acquireNextImage(&imageIndex);
-
-        if (result == VK_ERROR_OUT_OF_DATE_KHR) {
-            recreateSwapChain();
-            return;
-        }
-
-        if (result !=  VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
-            throw std::runtime_error("Failed to acquire swap chain image!");
-        }
-
-        recordCommandBuffer(imageIndex);
-        result = lardSwapChain->submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
-        if (result == VK_ERROR_OUT_OF_DATE_KHR || result == VK_SUBOPTIMAL_KHR || lardWindow.wasWindowResized()) {
-            lardWindow.resetWindowResizedFlag();
-            recreateSwapChain();
-            return;
-
-        }
-        if (result != VK_SUCCESS) {
-            throw std::runtime_error("Failed to present swap chain image!");
-        }
-    }
-
-    void FirstApp::sierpinski(std::vector<LardModel::Vertex> &vertices, int depth, glm::vec2 left, glm::vec2 right, glm::vec2 top) {
-        if (depth <= 0) {
-            vertices.push_back({top});
-            vertices.push_back({right});
-            vertices.push_back({left});
-        } else {
-            auto leftTop = 0.5f * (left + top);
-            auto rightTop = 0.5f * (right + top);
-            auto leftRight = 0.5f * (left + right);
-            sierpinski(vertices, depth - 1, left, leftRight, leftTop);
-            sierpinski(vertices, depth - 1, leftRight, right, rightTop);
-            sierpinski(vertices, depth - 1, leftTop, rightTop, top);
         }
     }
 }
--- first_app.hpp
+++ first_app.hpp
@@ -7,37 +7,30 @@
 #include "lard_game_object.hpp"
 #include "lard_pipeline.hpp"
 #include "lard_device.hpp"
-#include "lard_swap_chain.hpp"
+#include "lard_renderer.hpp"
 
 namespace lard {
     class FirstApp {
-        public:
-            FirstApp();
-            ~FirstApp();
-            FirstApp(const FirstApp &) = delete;
-            FirstApp &operator=(const FirstApp &) = delete;
-            static constexpr int WIDTH = 800;
-            static constexpr int HEIGHT = 600;
+    public:
+        FirstApp();
+        ~FirstApp();
+        FirstApp(const FirstApp&) = delete;
+        FirstApp& operator=(const FirstApp&) = delete;
+        static constexpr int WIDTH = 800;
+        static constexpr int HEIGHT = 600;
 
-            void run();
-        private:
-            void loadGameObjects();
-            void createPipelineLayout();
-            void createPipeline();
-            void createCommandBuffers();
-            void freeCommandBuffers();
-            void drawFrame();
-            void sierpinski(std::vector<LardModel::Vertex> &verticies, int depth, glm::vec2 left, glm::vec2 right, glm::vec2 top);
-            void recreateSwapChain();
-            void recordCommandBuffer(int imageIndex);
-            void renderGameObjects(VkCommandBuffer commandBuffer);
+        void run();
+    private:
+        void loadGameObjects();
+        void createPipelineLayout();
+        void createPipeline();
+        void renderGameObjects(VkCommandBuffer commandBuffer);
 
-            LardWindow lardWindow{WIDTH, HEIGHT, "Hello Vulkan!"};
-            LardDevice lardDevice{lardWindow};
-            std::unique_ptr<LardSwapChain> lardSwapChain;
-            std::unique_ptr<LardPipeline> lardPipeline;
-            VkPipelineLayout pipelineLayout;
-            std::vector<VkCommandBuffer> commandBuffers;
-            std::vector<LardGameObject> gameObjects;
+        LardWindow lardWindow{ WIDTH, HEIGHT, "Hello Vulkan!" };
+        LardDevice lardDevice{ lardWindow };
+        LardRenderer lardRenderer{ lardWindow, lardDevice };
+        std::unique_ptr<LardPipeline> lardPipeline;
+        VkPipelineLayout pipelineLayout;
+        std::vector<LardGameObject> gameObjects;
     };
 }
